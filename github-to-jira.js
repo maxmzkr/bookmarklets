1ca3a65a72290646f4ad1e8a98798c264fd26092
Max Mizikar <max.mizikar@nokia.com>
2019-05-16 09:03:54 -0400

    parse jira name instead of branch name

--- github-to-jira.js
+++ github-to-jira.js
@@ -2,5 +2,8 @@ javascript:(function() {
   var branchClasses = document.getElementsByClassName("commit-ref");
   var branchClass = branchClasses[branchClasses.length-1];
   var branchName = branchClass.textContent;
-  window.open("https://deepfield.atlassian.net/browse/"+branchName, "_target");
+  var branchSplits = branchName.split('-');
+  var jiraNameParts = branchSplits.slice(0, 2);
+  var jiraName = jiraNameParts.join('-');
+  window.open("https://deepfield.atlassian.net/browse/"+jiraName, "_blank");
 })();
